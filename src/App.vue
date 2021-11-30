d0c62eb2b976c054f8e7e32b49993825da2b83eb
lack-io <598223084@qq.com>
2021-11-30 22:00:19 +0800

    ant-design-vue

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <HelloWorld/>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
 
-@Options({
+export default defineComponent({
   components: {
     HelloWorld,
   },
-})
-export default class App extends Vue {}
+
+  setup() {
+    return {};
+  },
+});
 </script>
 
 <style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import {
+    Button,
+} from 'ant-design-vue';
+
+const app = createApp(App)
+
+app.use(
+    Button,
+)
+
+app.mount('#app')
