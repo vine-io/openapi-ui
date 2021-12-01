72e80bd38e8405a6f55fc9baff32ed42f92b2c94
lack <598223084@qq.com>
2021-12-01 18:43:27 +0800

    merge

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,10 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+<!--  <img alt="Vue logo" src="./assets/logo.png">-->
   <HelloWorld/>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
+<script setup lang="ts">
 import HelloWorld from "./components/HelloWorld.vue";
-
-export default defineComponent({
-  components: {
-    HelloWorld,
-  },
-
-  setup() {
-    return {};
-  },
-});
 </script>
 
 <style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,31 +1,19 @@
 <template>
   <a-button @click="count += 1">{{ count }}</a-button>
+
+  <div class="header">header {{count}}</div>
 </template>
 
 
+<script setup lang="ts">
+import {ref} from "vue";
 
-
-<script lang="ts">
-import { defineComponent, ref } from "vue";
-
-  setup() {
-    let count = ref(0);
-
-    return {
-      count,
-    };
-  },
-});
+let count = ref(0);
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="less" scoped>
 .header {
-  border: 1px solid;
-
-  .span {
-    display: block;
-    font-size: 10px;
-  }
+  font-size: 10px;
 }
 </style>
